84853f1757f373eeb0fb912160e68216f0ee9222
rowanam <rowanamaas@gmail.com>
2023-12-02 23:47:56 +0000

    Change map provider to OpenStreetMaps with Leaflet

--- map/views.py
+++ map/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.views import View
-import os
 
 
 class Map(View):
     def get(self, request):
-        maps_api_key = os.environ.get("GOOGLE_MAPS_API_KEY")
         return render(
             request,
-            "map.html",
-            {"maps_api_key": maps_api_key},
+            "map.html"
         )
