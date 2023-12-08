f241698253e8ab3c2413540ce28db5afe21aae20
rowanam <rowanamaas@gmail.com>
2023-12-08 03:49:34 +0000

    feat: display trip dates and increase trip map size

--- static/js/trip_map.js
+++ static/js/trip_map.js
@@ -1,30 +1,40 @@
-// SAMPLE MAP
+function setMapHeight() {
+    // set map aspect ratio to 3:2, based on width
+    $("#map").height($("#map").width() * 2 / 3);
+}
 
-// initialize Leaflet
-var map = L.map('map').setView({
-    lon: 0,
-    lat: 0
-}, 2);
+$(document).ready(function () {
+    // set map height on page load and window resize
+    setMapHeight();
+    $(window).resize(setMapHeight);
 
-// add the OpenStreetMap tiles
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-}).addTo(map);
+    // RENDER MAP
+    // initialize Leaflet
+    var map = L.map('map').setView({
+        lon: 0,
+        lat: 0
+    }, 2);
 
-// show the scale bar on the lower left corner
-L.control.scale({
-    imperial: true,
-    metric: true
-}).addTo(map);
+    // add the OpenStreetMap tiles
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
 
-let coordinates = JSON.parse(document.getElementById('coordinates').textContent);
+    // show the scale bar on the lower left corner
+    L.control.scale({
+        imperial: true,
+        metric: true
+    }).addTo(map);
 
-// create a red polyline from the array of LatLng points
-var polyline = L.polyline(coordinates, {
-    color: 'orange',
-    weight: 2,
-}).addTo(map);
+    let coordinates = JSON.parse(document.getElementById('coordinates').textContent);
 
-// zoom the map to the polyline
-map.fitBounds(polyline.getBounds());
+    // create a red polyline from the array of LatLng points
+    var polyline = L.polyline(coordinates, {
+        color: 'orange',
+        weight: 2,
+    }).addTo(map);
+
+    // zoom the map to the polyline
+    map.fitBounds(polyline.getBounds());
+});
