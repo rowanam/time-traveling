d6eed36a9623da04f6217431dcf292d045f4f776
rowanam <rowanamaas@gmail.com>
2023-12-18 09:18:06 +0000

    fix: image uploading

--- timetraveling/settings.py
+++ timetraveling/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 if os.path.isfile("env.py"):
     import env
