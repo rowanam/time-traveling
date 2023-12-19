06c621335a66612a6a1bc212dc71a981bf74d55c
rowanam <rowanamaas@gmail.com>
2023-12-19 11:27:21 +0000

    test: create tests for trip form

--- trips/tests/test_forms.py
+++ trips/tests/test_forms.py
@@ -1,21 +1,30 @@
 from django.test import TestCase
-
-# Get user model from settings
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-from ..models import Trip
-from ..forms import LocationInlineFormSet
+from datetime import datetime
+from ..forms import TripForm
 
 
-# class TestLocationInlineFormSet(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         test_user = User.objects.create_user(username="testuser", password="Password*1")
-#         Trip.objects.create(title="Test Trip", user=test_user)
+class TestTripForm(TestCase):
+    def test_start_date_requires_end_date(self):
+        start_date_object = datetime.strptime("Nov 13, 2023", "%b %d, %Y")
+        form = TripForm({"title": "Test", "start_date": start_date_object})
+        self.assertFalse(form.is_valid())
+        self.assertIn("end_date", form.errors.keys())
 
-#     def test_delete_field_is_hidden(self):
-#         trip = Trip.objects.get(id=1)
-#         formset = LocationInlineFormSet(instance=trip)
-#         # for form in formset:
-#         #     self.assertEqual(form.fields["DELETE"].widget, forms.HiddenInput())
+    def test_end_date_requires_start_date(self):
+        end_date_object = datetime.strptime("Dec 13, 2023", "%b %d, %Y")
+        form = TripForm({"title": "Test", "end_date": end_date_object})
+        self.assertFalse(form.is_valid())
+        self.assertIn("start_date", form.errors.keys())
+
+    def test_end_date_must_be_after_start_date(self):
+        start_date_object = datetime.strptime("Dec 13, 2023", "%b %d, %Y")
+        end_date_object = datetime.strptime("Nov 13, 2023", "%b %d, %Y")
+        form = TripForm(
+            {
+                "title": "Test",
+                "start_date": start_date_object,
+                "end_date": end_date_object,
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn("end_date", form.errors.keys())
